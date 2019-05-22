a8929f7aa376938542be5ac76bd59caa3163a26e
wangzx <wangzx@yuminsoft.com>
2019-05-22 18:11:40 +0800

    初始化

--- web/src/main/java/com/wzx/pro/web/WebApplication.java
+++ web/src/main/java/com/wzx/pro/web/WebApplication.java
@@ -1,13 +1,22 @@
 package com.wzx.pro.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 启动类
+ * @author wangzx
+ * @date 2019/5/22 10:00
+ *
+ */
 
 @SpringBootApplication
+@Slf4j
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+        log.info("==============WebApplication Start Success=====================");
     }
 
 }
