b04c585c40b926423de3ce7e42f848d23430dd92
wangzx <wangzx@yuminsoft.com>
2019-06-21 21:04:42 +0800

    接druid

--- web/src/main/java/com/wzx/pro/web/WebApplication.java
+++ web/src/main/java/com/wzx/pro/web/WebApplication.java
@@ -2,14 +2,17 @@ package com.wzx.pro.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
 /**
  * 启动类
  * @author wangzx
  * @date 2019/5/22 10:00
  *
  */
-
+@MapperScan(basePackages = "com.wzx.pro.dao")
 @SpringBootApplication
 @Slf4j
 public class WebApplication {
