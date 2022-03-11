4be9080448cb2858d15a8236383f5fae7fa7986c
wangzhenxing <709193115@qq.com>
2022-03-11 22:00:08 +0800

    03-11-home

--- web/src/main/java/com/wzx/pro/question/tik/Q10_129_求根节点到叶节点数字之和.java
+++ web/src/main/java/com/wzx/pro/question/tik/Q10_129_求根节点到叶节点数字之和.java
@@ -2,6 +2,8 @@ package com.wzx.pro.question.tik;
 
 public class Q10_129_求根节点到叶节点数字之和 {
 
+    private static int summer = 0;
+
     private static int solution(TreeNode root) {
         return back(root, 0);
     }
@@ -24,13 +26,36 @@ public class Q10_129_求根节点到叶节点数字之和 {
         TreeNode root_right_1 = new TreeNode(0);
         TreeNode root_left_1_1 = new TreeNode(5);
         TreeNode root_left_1_2 = new TreeNode(1);
+        TreeNode root_left_1_3 = new TreeNode(10);
 
         root.left = root_left_1;
         root.right = root_right_1;
         root_left_1.left = root_left_1_1;
         root_left_1.right = root_left_1_2;
+        root_left_1_2.right = root_left_1_3;
 
         int solution = solution(root);
         System.out.println(solution);
+
+        int i = solutionSum(root);
+        System.out.println(i);
+    }
+
+
+    private static int solutionSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        aa(root, 0);
+        return summer;
+    }
+
+    private static void aa(TreeNode root, int height) {
+        if (root == null) {
+            return;
+        }
+        summer += root.val;
+        aa(root.left, height + 1);
+        aa(root.right, height + 1);
     }
 }
