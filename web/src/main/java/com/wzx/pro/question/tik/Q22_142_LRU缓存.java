c2f04064f6ed4cfe7dd950e0da00508d9c664b21
wangzhenxing <709193115@qq.com>
2022-03-16 09:19:20 +0800

    03-16-home

--- web/src/main/java/com/wzx/pro/question/tik/Q22_142_LRU缓存.java
+++ web/src/main/java/com/wzx/pro/question/tik/Q22_142_LRU缓存.java
@@ -2,4 +2,27 @@ package com.wzx.pro.question.tik;
 
 public class Q22_142_LRU缓存 {
 
+    private static class LRUCache {
+        private int pacacity;
+
+        private LRUCache(int capacity) {
+            this.pacacity = capacity;
+        }
+    }
+
+    private int get(int key) {
+
+
+        return 0;
+    }
+
+    private void put(int key, int value) {
+
+    }
+
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(1);
+        System.out.println(lruCache.pacacity);
+    }
+
 }
